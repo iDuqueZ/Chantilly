2bf3cbae54e6ff2078c0a8b07638e0988b5d95f2
= <=>
2022-04-27 13:03:44 -0500

    archivos extras

--- backend/src/models/CarritoMdl.js
+++ backend/src/models/CarritoMdl.js
@@ -5,7 +5,7 @@ const CarritoSchema = new Schema ({
     susProductos: {type:Array},
     metodoPago: {type:String, default:["Efectivo"]},
     fecha: {type:Date, required: [true, 'La fecha es obligatorio']},
-    hora: {type:Time, required: [true, 'La hora es obligatorio']},
+    hora: {type:Date, required: [true, 'La hora es obligatorio']},
     idUsuario: {type:Number, reuiere: true},
     estadoPedido: {type:String, default:["En espera"]}
 })
