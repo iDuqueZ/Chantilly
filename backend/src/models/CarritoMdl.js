8f56b60cae0a605b0557e1145dea9a60087059aa
Juan David G <juandavidg@live.com.ar>
2022-04-30 14:32:04 -0500

    Se crÃeó el primer controlador -AdminCtrl

--- backend/src/models/CarritoMdl.js
+++ backend/src/models/CarritoMdl.js
@@ -3,11 +3,11 @@ const {Schema} = mongoose
 
 const CarritoSchema = new Schema ({
     susProductos: {type:Array},
-    metodoPago: {type:String, default:["Efectivo"]},
-    fecha: {type:Date, required: [true, 'La fecha es obligatorio']},
-    hora: {type:Date, required: [true, 'La hora es obligatorio']},
+    metodoPago: {type:String, default:"Efectivo"},
+    fecha: {type:Date, default:now},
+    hora: {type:Date, default:now},
     idUsuario: {type:Number, reuiere: true},
-    estadoPedido: {type:String, default:["En espera"]}
+    estadoPedido: {type:String, default:"En espera"}
 })
 
 module.exports = mongoose.model('carrito', CarritoSchema)
